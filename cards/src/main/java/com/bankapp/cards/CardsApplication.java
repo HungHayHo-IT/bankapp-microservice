3ba2f9c322602d58f3493c18787b8b5b41591d59
HungHayHo <nguyenphuocnhahungl97@gmail.com>
2026-06-22 11:36:32 +0700

    up

---
+++ cards/src/main/java/com/bankapp/cards/CardsApplication.java
@@ -0,0 +1,40 @@
+package com.bankapp.cards;
+
+import com.bankapp.cards.entity.Cards;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
+@EnableJpaRepositories("com.eazybytes.cards.repository")
+@EntityScan("com.eazybytes.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = " Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "HungHayHo-IT",
+						email = "nguyenphuocnhahungl97@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = " Cards microservice REST API Documentation"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
